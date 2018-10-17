65f6e39d533bc9954d86ec02245a4bf7c4a08d77
Santiago Vottero <caonsv@gmail.com>
2018-10-17 14:42:15 -0300

    search inline prompt fecth

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Importá productsFetcher para que este script no se rompa!
 const fetchProducts = require('./src/productsFetcher')
 
-fetchProducts('ipod');
+fetchProducts();
--- src/productsFetcher.js
+++ src/productsFetcher.js
@@ -2,18 +2,29 @@ const fetch = require('node-fetch')
 const chalk = require('chalk');
 const log = console.log;
 const fs = require('fs');
+const prompt = require('prompt');
 
 
-function fetchProducts(query) {
-	var url = "https://api.mercadolibre.com/sites/MLA/search?limit=10&q="
-	
-	fetch(`${url}${query}`)
-		.then(response => response.json())
-		.then(({ results = [] }) => results.map((data, i) =>{
+function fetchProducts() {
+	const url = "https://api.mercadolibre.com/sites/MLA/search?limit=10&q="
+
+	prompt.start();
+	prompt.get({
+		properties: {
+			busqueda: {
+				description: chalk.green("¿Qué desea buscar?")
+			}
+		}
+	}, function (err, result) {
+
+		fetch(`${url}${result.busqueda}`)
+			.then(response => response.json())
+			.then(({ results = [] }) => results.map((data, i) => {
+				log(`${chalk.cyan(i + 1)} - ${data.title} - ${chalk.red('$'+data.price)}`)
 				return `<li>${i + 1} - ${data.title} - $${data.price}</li>`
 			})
-		)
-		.then(listing => `
+			)
+			.then(listing => `
 			<html>
 				<head></head>
 				<body>
@@ -23,13 +34,15 @@ function fetchProducts(query) {
 				</body>
 			</html>
 		`)
-		.then((html) => {
-			fs.writeFile("temp/demo.html", html, err => {
-				if (err) {
-					return console.log(err);
-				}
-			});
-		})
+			.then((html) => {
+				fs.writeFile("temp/demo.html", html, err => {
+					if (err) {
+						return console.log(err)
+					}
+				})
+			})
+	})
+
 }
 
 module.exports = fetchProducts
